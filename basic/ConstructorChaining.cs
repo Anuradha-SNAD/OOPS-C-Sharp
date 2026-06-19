e05e73b0f2c93d7cc05db8382ee952c229dc53ba
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-06-19 19:51:34 +0530

    Add files via upload

---
+++ basic/ConstructorChaining.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.OOPS_Basics
+{
+    public class ConstructorChaining
+    {
+        public string Name {  get; set; }
+        public int id { get; set; }
+        public int marks {  get; set; }
+
+        public ConstructorChaining(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public ConstructorChaining(string Name,int id) : this(Name)
+        {
+            this.id = id;
+        }
+        public ConstructorChaining(string Name, int id,int marks) : this(Name,id)
+        {
+            this.marks = marks;
+        }
+    }
+}
---
+++ basic/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.OOPS_Basics
+{
+    internal class Student
+    {
+        int id = 101;
+        string name = "raju";
+        public Student()
+        {
+            Console.WriteLine("Id = " + id);
+            Console.WriteLine("Name = " + name);
+        }
+        static Student()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+    }
+    class Student1
+    {
+        public int id;
+        public string name;
+        public Student1()
+        {
+            Console.WriteLine("No-Argument Contructor");
+        }
+       public Student1(int id,string name)//No-Arguments Constructor
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
