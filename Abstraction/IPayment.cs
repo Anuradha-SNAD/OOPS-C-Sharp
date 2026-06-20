216cc7ca46c473bddb4134e2c47c08cbed7e24a4
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-06-20 19:56:27 +0530

    Add files via upload

---
+++ Abstraction/IPayment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.Interface
+{
+    public interface IPayment
+    {
+        void Pay();
+    }
+    public class PhonePo : IPayment
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Payment via PhonePe");
+        }
+    }
+    public class GooglePay : IPayment
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Payment via GooglePay");
+        }
+    }
+    public class Swiggy
+    {
+        public void ProcessPayment(IPayment payment)
+        {
+            payment.Pay();
+        }
+    }
+}
