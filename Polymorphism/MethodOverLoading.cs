2a1fbefb30f3edad63d5289a957e75d7bc7a5c85
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-06-20 19:55:18 +0530

    Add files via upload

---
+++ Polymorphism/MethodOverLoading.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.Polymorphism
+{
+    public class MethodOverLoading
+    {
+        public int Add(int a)
+        {
+            return a;
+        }
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public int Add(int a,int b,int c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ Polymorphism/MethodOverriding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.Polymorphism
+{
+    public class MethodOverriding
+    {
+        public virtual  void Method()
+        {
+            Console.WriteLine("Parent Method...!");
+        }
+    }
+    public class A : MethodOverriding
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Child A Method");
+        }
+
+    }
+    public class B : MethodOverriding
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Child B Method");
+        }
+
+    }
+}
