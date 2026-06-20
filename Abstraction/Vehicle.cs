28c69a9e05052b510846672856af1b93f0eb996d
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-06-20 19:56:04 +0530

    Add files via upload

---
+++ Abstraction/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.Abstraction
+{
+   public abstract class Vehicle
+    {
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle Constructor");
+        }
+        public abstract void Start();
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopped..!");
+        }
+    }
+    public class V : Vehicle
+    {
+        public V()
+        {
+            Console.WriteLine("V Constructor..!");
+        }
+        public override void Start()
+        {
+            Console.WriteLine("Started..!");
+        }
+    }
+}
